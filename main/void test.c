f91cc65f25e4e2ad59d6ac69e050efe107bf48ff
dhy <m18256112005@163.com>
2025-11-29 15:53:21 +0800

    心倒计时

--- main/void test.c
+++ main/void test.c
@@ -1,11 +1,29 @@
+#include <stdio.h>
 #include <windows.h>
 int main(void)
 {   
-    int i = 5;
+    int i ;
+    float x, y;
+    printf("请输入响铃次数：");
+    scanf("%d",&i);
+    for(y = 1.3f; y > -1.1f; y -= 0.08f) {
+        for(x = -1.8f; x < 1.8f; x += 0.05f) {
+            // 修改爱心方程参数，使其更宽更饱满
+            float a = x*x + y*y - 1;
+            if(a*a*a - (x*x)*(y*y*y) <= 0.0f) {
+                printf("* ");
+            } else {
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+    Sleep(800);//延时0.8秒
     while(i)
     {   Beep(3500, 200);
         Beep(3500, 200);
         Sleep(800);//延时0.8秒
         i--;
     }
+    return 0;
 }
